6b15bc8799da1983126237800147c361bd65b68f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-10-08 12:09:26 -0700

    1-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts a Flask Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -9,17 +9,13 @@ from os import environ
 from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
-
 
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -35,13 +31,14 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
+    # Generate a unique cache_id
+    cache_id = str(uuid.uuid4())
+
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
-    cache_id = str(uuid.uuid4())
-    return render_template('0-hbnb.html', cache_id=cache_id)
-
+                           places=places,
+                           cache_id=cache_id)
 
 if __name__ == "__main__":
     """ Main Function """
